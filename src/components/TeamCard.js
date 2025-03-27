73e191d004f49aa08c847ecccc7799328ee10b87
Deep Sutariya <117342002+deep-sutariya@users.noreply.github.com>
2025-03-27 10:17:09 +0530

    Update TeamCard.js

--- src/components/TeamCard.js
+++ src/components/TeamCard.js
@@ -2,7 +2,7 @@ import TranslateFragment from "./Animations/TranslateFragment"
 
 const TeamCard = ({ name, role }) => {
     return (
-        <div className=" duration-150">
+        <div className=" duration-150 w-1/3">
             <TranslateFragment y="100">
                 <div className=" flex-col gap-y-4 px-2 md:px-8 lg:px-12 text-center py-2 md:py-4 border backdrop-blur-md hover:scale-105 duration-150 cursor-pointer">
                     <div className="text-[11px] md:text-lg lg:text-2xl font-heading font-semibold">{name}</div>
