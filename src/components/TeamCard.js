14846011e0745bdf06982f74ba24504dd75e6f60
Deep Sutariya <117342002+deep-sutariya@users.noreply.github.com>
2025-03-27 10:28:32 +0530

    Update TeamCard.js

--- src/components/TeamCard.js
+++ src/components/TeamCard.js
@@ -5,7 +5,7 @@ const TeamCard = ({ name, surname, role }) => {
         <div className=" duration-150 w-1/3">
             <TranslateFragment y="100">
                 <div className=" flex-col gap-y-4 px-2 md:px-8 lg:px-12 text-center py-2 md:py-4 border backdrop-blur-md hover:scale-105 duration-150 cursor-pointer">
-                <div className="flex flex-col justify-center items-center gap-y-2"> 
+                <div className="flex flex-col justify-center items-center"> 
                     <div className="text-[11px] md:text-lg lg:text-2xl font-heading font-semibold">{name}</div>
                     <div className="text-[11px] md:text-lg lg:text-2xl font-heading font-semibold">{surname}</div>
                 </div>
